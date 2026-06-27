e6af60c07432f650a46741b676277b96a2432ea1
ashvin <172140357+ashvin-parmar@users.noreply.github.com>
2026-06-28 04:13:58 +0530

    sprintboot: used spring initializr to generate skeleton structure of application, and install using maven

---
+++ msgboard/src/test/java/com/ashvin/msgboard/MsgboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashvin.msgboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
