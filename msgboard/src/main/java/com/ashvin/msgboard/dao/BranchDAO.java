166d132ac9eeeeebf3d9ba9e9fe8cd2797ed677d
ashvin <172140357+ashvin-parmar@users.noreply.github.com>
2026-07-08 19:01:58 +0530

    msgboard: branch and semester database dao crud operations

--- msgboard/src/main/java/com/ashvin/msgboard/dao/BranchDAO.java
+++ msgboard/src/main/java/com/ashvin/msgboard/dao/BranchDAO.java
@@ -2,6 +2,7 @@ package com.ashvin.msgboard.dao;
 
 import java.sql.*;
 import com.ashvin.msgboard.dto.*;
+import java.util.*;
 
 /**
  * @author Ashvin
@@ -10,7 +11,9 @@ import com.ashvin.msgboard.dto.*;
  */
 public class BranchDAO 
 {
-public BranchDAO(){};
+public BranchDAO()
+{
+}
 public void add(Branch branch) throws DAOException
 {
 if(branch==null) throw new DAOException("branch required");
@@ -21,23 +24,151 @@ if(name.isBlank()) throw new DAOException("branch name required");
 Connection connection=DAOConnection.getConnection();
 try
 {
-PreparedStatement preparedStatement=connection.prepareStatement("insert into branch (name) values(?)",Statement.RETURN_GENERATED_KEYS);
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+preparedStatement=connection.prepareStatement("select code from branch where name=?");
+preparedStatement.setString(1,name);
+resultSet=preparedStatement.executeQuery();
+boolean valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("semester name: "+name+" already exists");
+}
+
+preparedStatement=connection.prepareStatement("insert into branch (name) values(?)",Statement.RETURN_GENERATED_KEYS);
 preparedStatement.setString(1,name);
 preparedStatement.executeUpdate();
-ResultSet resultSet=preparedStatement.getGeneratedKeys();
+resultSet=preparedStatement.getGeneratedKeys();
 resultSet.next();
+Integer code=resultSet.getInt(1);
+branch.setCode(code);
 resultSet.close();
 preparedStatement.close();
 connection.close();
-
-Integer code=resultSet.getInt(1);
-branch.setCode(code);
-
 }catch(SQLException sqlException)
 {
 System.out.println(sqlException);       //later on add to log messages
 throw new DAOException("Unable to add branch: "+name);
 }
-}    
+}
+public void update(Branch branch) throws DAOException
+{
+if(branch==null) throw new DAOException("branch required");
+int code=branch.getCode();
+//if(code<=0) throw new DAOException("invalid branch code: "+code);
+if(code<=0) throw new DAOException("invalid branch data provided, not found");
+if(branch.getName()==null) throw new DAOException("branch name required");
+String name=branch.getName();
+if(name.isBlank()) throw new DAOException("branch name required");
+
+Connection connection=DAOConnection.getConnection();
+try
+{
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+preparedStatement=connection.prepareStatement("select * from branch where code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+boolean valid=resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+//throw new DAOException("invalid branch code: "+code);
+throw new DAOException("invalid branch data provided, not found");
+}
+preparedStatement=connection.prepareStatement("select * from student_branch_mapping where branch_code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("unable to update record of branch, since respective branch is assigned to student data");
 }
 
+preparedStatement=connection.prepareStatement("update branch set name=? where code=?");
+preparedStatement.setString(1,name);
+preparedStatement.setInt(2,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);       //later on add to log messages
+throw new DAOException("Unable to update branch");
+}
+}
+public void delete(int code) throws DAOException
+{
+if(code<=0) throw new DAOException("invalid branch data provided, not found");
+Connection connection=DAOConnection.getConnection();
+try
+{
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+preparedStatement=connection.prepareStatement("select code from branch where code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+boolean valid=resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("invalid branch data provided, not found");
+}
+preparedStatement=connection.prepareStatement("select * from student_branch_mapping where branch_code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("unable to delete record of branch, since respective branch is assigned to student data");
+}
+preparedStatement=connection.prepareStatement("delete from branch where code=?");
+preparedStatement.setInt(1,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);
+throw new DAOException("unable to delete record of branch");
+}
+}
+public List<Branch> getBranches() throws DAOException
+{
+List<Branch> branches=new ArrayList<>();
+Branch branch;
+int code;
+String name;
+Connection connection=DAOConnection.getConnection();
+try
+{
+PreparedStatement preparedStatement=connection.prepareStatement("select * from branch order by name");
+ResultSet resultSet=preparedStatement.executeQuery();
+while(resultSet.next())
+{
+code=resultSet.getInt("code");
+name=resultSet.getString("name").trim();
+branch=new Branch();
+branch.setCode(code);
+branch.setName(name);
+branches.add(branch);
+}
+resultSet.close();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);
+throw new DAOException("unable to get branch record");
+}
+return branches;
+}
+}
--- msgboard/src/main/java/com/ashvin/msgboard/dao/SemesterDAO.java
+++ msgboard/src/main/java/com/ashvin/msgboard/dao/SemesterDAO.java
@@ -2,6 +2,8 @@ package com.ashvin.msgboard.dao;
 
 import java.sql.*;
 import com.ashvin.msgboard.dto.*;
+import java.util.*;
+
 /**
  * @author Ashvin
  * @since 2026-07-08
@@ -9,7 +11,9 @@ import com.ashvin.msgboard.dto.*;
  */
 public class SemesterDAO 
 {
-public SemesterDAO(){};
+public SemesterDAO()
+{
+}
 public void add(Semester semester) throws DAOException
 {
 if(semester==null) throw new DAOException("semester required");
@@ -19,10 +23,23 @@ if(name.isBlank()) throw new DAOException("semester name required");
 Connection connection=DAOConnection.getConnection();
 try
 {
-PreparedStatement preparedStatement=connection.prepareStatement("insert into semester (name) values(?)",Statement.RETURN_GENERATED_KEYS);
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+preparedStatement=connection.prepareStatement("select code from semester where name=?");
+preparedStatement.setString(1,name);
+resultSet=preparedStatement.executeQuery();
+boolean valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("semester name: "+name+" already exists");
+}
+
+preparedStatement=connection.prepareStatement("insert into semester (name) values(?)",Statement.RETURN_GENERATED_KEYS);
 preparedStatement.setString(1,name);
 preparedStatement.executeUpdate();
-ResultSet resultSet=preparedStatement.getGeneratedKeys();
+resultSet=preparedStatement.getGeneratedKeys();
 resultSet.next();
 Integer code=resultSet.getInt(1);
 resultSet.close();
@@ -36,5 +53,126 @@ System.out.println(sqlException);
 throw new DAOException("unable to add semester: "+name);
 }
 }
+
+public void update(Semester semester) throws DAOException
+{
+//validation
+if(semester==null) throw new DAOException("semester required");
+int code=semester.getCode();
+if(code<=0) throw new DAOException("invalid semester data provided, not found");
+String name=semester.getName();
+if(name==null || name.isBlank()) throw new DAOException("semester name required");
+
+Connection connection=DAOConnection.getConnection();
+try
+{
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+boolean valid;
+preparedStatement=connection.prepareStatement("select code from semester where code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("invalid semester data provided, not found");
 }
 
+preparedStatement=connection.prepareStatement("select semester_code from student_semester_mapping where semester_code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("unable to update semester record, since semester assigned to student record");
+}
+
+preparedStatement=connection.prepareStatement("update semester set name=? where code=?");
+preparedStatement.setString(1,name);
+preparedStatement.setInt(2,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);
+throw new DAOException("unable to update semester record");
+}
+}
+public void delete(int code) throws DAOException
+{
+if(code<=0) throw new DAOException("invalid semester data provided, not found");
+Connection connection=DAOConnection.getConnection();
+try
+{
+PreparedStatement preparedStatement;
+ResultSet resultSet;
+boolean valid;
+preparedStatement=connection.prepareStatement("select code from semester where code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("invalid semester data provided, not found");
+}
+
+preparedStatement=connection.prepareStatement("select semester_code from student_semester_mapping where semester_code=?");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+valid=!resultSet.next();
+resultSet.close();
+preparedStatement.close();
+if(!valid)
+{
+throw new DAOException("unable to delete semester record, since semester assigned to student record");
+}
+
+preparedStatement=connection.prepareStatement("delete from semester where code=?");
+preparedStatement.setInt(1,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);
+throw new DAOException("unable to delete semester record");
+}
+}
+public List<Semester> getSemesters() throws DAOException
+{
+List<Semester> semesters=new ArrayList<>();
+Connection connection=DAOConnection.getConnection();
+Semester semester;
+int code;
+String name;
+try
+{
+PreparedStatement preparedStatement=connection.prepareStatement("select * from semester order by name");
+ResultSet resultSet=preparedStatement.executeQuery();
+while(resultSet.next())
+{
+code=resultSet.getInt("code");
+name=resultSet.getString("name").trim();
+semester=new Semester();
+semester.setCode(code);
+semester.setName(name);
+semesters.add(semester);
+}
+resultSet.close();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException);
+throw new DAOException("unable to get semester record");
+}
+return semesters;
+}
+}
