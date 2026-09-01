7b4d591679951c533a3f0784db83191124669bf5
ashvin <172140357+ashvin-parmar@users.noreply.github.com>
2026-09-01 11:35:16 +0530

    learn: defining @Scope for components {singleton, prototype, application, session}

--- learn/src/main/java/com/ashvin/parmar/learn/core/management/HockeyCouch.java
+++ learn/src/main/java/com/ashvin/parmar/learn/core/management/HockeyCouch.java
@@ -2,10 +2,12 @@ package com.ashvin.parmar.learn.core.management;
 
 import org.springframework.stereotype.*;
 import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;    //Scope available in both context and beans
 
 @Component("hockeyCouchManagement")
 @Primary        //Only one from all Couch implements may be set as primary. It treat as default selection for Couch type of Component selection
-@Lazy(false)
+@Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class HockeyCouch implements Couch 
 {
 public HockeyCouch()
--- learn/src/main/java/com/ashvin/parmar/learn/core/management/SportController.java
+++ learn/src/main/java/com/ashvin/parmar/learn/core/management/SportController.java
@@ -3,9 +3,13 @@ package com.ashvin.parmar.learn.core.management;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
 
+
 @Controller
+@Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SportController
 {
 private Couch couch;
