82fb1078607396e9635b637e0be8a3d4ca6c76a8
ashvin <172140357+ashvin-parmar@users.noreply.github.com>
2026-09-11 14:58:50 +0530

    leran: initial setup for jpa startup and jdbc connection driver

--- learn/src/main/java/com/ashvin/parmar/learn/LearnApplication.java
+++ learn/src/main/java/com/ashvin/parmar/learn/LearnApplication.java
@@ -1,7 +1,8 @@
 package com.ashvin.parmar.learn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.*;
+import org.springframework.context.annotation.*;
 
 @SpringBootApplication(scanBasePackages={"com.ashvin.parmar.learn","com.ashvin.parmar.custom"})
 public class LearnApplication {
@@ -9,5 +10,13 @@ public class LearnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
 	}
+
+    @Bean 
+    public CommandLineRunner commandLineRunner(String[] args)
+    {
+        return runner -> {
+            System.out.println("Hello, Here we start our learning JPA journey!");
+        };
+    }
 
 }
